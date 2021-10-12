25ae89fa2e6c5be843ccd0f0adbb7479ea490a12
Nguyen-Ngoc-Hung <ngochung732001@gmail.com>
2021-10-13 00:15:59 +0700

    resize

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ import pygame,sys
 import random
 pygame.init()
 
-screen=pygame.display.set_mode((768,1024))
+screen=pygame.display.set_mode((384,512))
 
 pygame.display.set_caption("Flappy bird")
 background = pygame.image.load("images/background.png")
@@ -18,14 +18,14 @@ clock = pygame.time.Clock()
 x=0
 y=0
 voCanh=0
-posBird = 400.0
+posBird = 200.0
 print(sys.platform)
 
 def drawBackground():
     global y
-    if y>760: y=0
+    if y>380: y=0
     screen.blit(background,(0-y,0))
-    screen.blit(background,(760-y,0))
+    screen.blit(background,(380-y,0))
     y+=1
 
 
@@ -34,13 +34,13 @@ def drawBird():
     global voCanh
     global alpha
     if voCanh < 10:
-        screen.blit(pygame.transform.rotate(bird1,int(alpha)),(200,int(posBird)))
+        screen.blit(pygame.transform.rotate(bird1,int(alpha)),(100,int(posBird)))
         voCanh+=1
     elif voCanh < 20:
-        screen.blit(pygame.transform.rotate(bird2,int(alpha)),(200,int(posBird)))
+        screen.blit(pygame.transform.rotate(bird2,int(alpha)),(100,int(posBird)))
         voCanh+=1
     elif voCanh < 30:
-        screen.blit(pygame.transform.rotate(bird3,int(alpha)),(200,int(posBird)))
+        screen.blit(pygame.transform.rotate(bird3,int(alpha)),(100,int(posBird)))
         voCanh +=1
         if voCanh >= 30: voCanh =0
 
@@ -50,21 +50,22 @@ def drawPipe(n):
     global speedPipe
     global mangOng
     screen.blit(pipe,(mangOng[n][0],mangOng[n][1]))
-    screen.blit(pygame.transform.rotate(pipe, 180),(mangOng[n][0],mangOng[n][1]-doRongOng-793))
-    mangOng[n][0] -=4
+    screen.blit(pygame.transform.rotate(pipe, 180),(mangOng[n][0],mangOng[n][1]-doRongOng-397))
+    mangOng[n][0] -=2
 
 def drawPipes():
     drawPipe(0)
     drawPipe(1)
     drawPipe(2)
     drawPipe(3)
+
 def drawBase():
     global x
-    if x >= 768:
+    if x >= 384:
         x=0
-    screen.blit(floor,(0-x,896))
-    screen.blit(floor,(768-x,896))
-    x += 4
+    screen.blit(floor,(0-x,448))
+    screen.blit(floor,(384-x,448))
+    x += 2
 
 def vaCham():
     global gameOver
@@ -75,20 +76,20 @@ def vaCham():
     if posBird + 90 - mangOng[0][0] < 228:
         gameOver = True
 
-giaToc=0.6
+giaToc=0.4
 doRoi = 0
 alpha=30.0
-khoangCachOng = 400
-doRongOng = 250
+khoangCachOng = 200
+doRongOng = 125
 alphaX = 50
 speedPipe = 0
-posPipe = 768
+posPipe = 384
 mangOng = []
 gameOver = False
 for i in range(1,5):
     temp = []
     temp.append(posPipe)
-    temp.append(random.randint(300,800))
+    temp.append(random.randint(150,400))
     mangOng.append(temp)
     posPipe += khoangCachOng
 
@@ -100,26 +101,15 @@ while True:
             pygame.quit()
             sys.exit()
 
-        if (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE)  or event.type ==5:
-            doRoi = -12
+        if (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE)  or event.type == 5:
+            doRoi = -7
             alphaX = 50
 
     drawBackground()
     drawPipes()
     drawBase()
     drawBird()
-
-
-
-    vaCham()
-    if gameOver == True:
-        drawBackground()
-        drawPipes()
-        drawBase()
-        drawBird()
-        print("Game Over")
-        gameOver = False
-
+    print(clock)
     doRoi += giaToc
     posBird += doRoi
     alphaX -=1.5
@@ -128,13 +118,13 @@ while True:
     alpha = alphaX
     if alpha > 30: 
         alpha =30
-    if posBird > 832:
-        posBird = 832
-    if mangOng[0][0] < -138:
+    if posBird > 416:
+        posBird = 416
+    if mangOng[0][0] < -69:
         del mangOng[0]
         temp = []
         temp.append(mangOng[2][0] + khoangCachOng)
-        temp.append(random.randint(300,800))
+        temp.append(random.randint(150,400))
         mangOng.append(temp)
     pygame.display.update()
     clock.tick(40)
